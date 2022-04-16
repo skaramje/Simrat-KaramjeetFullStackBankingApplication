327425a17300eec505944a39cd3b15cf387f0ce3
skaramje <skaramje@gmail.com>
2022-04-16 03:05:50 -0600

    index copy

--- index.js
+++ index.js
@@ -1,104 +1,45 @@
-var express = require('express');
-var app     = express();
-var cors    = require('cors');
-var dal     = require('./dal.js');
-const e     = require('express');
+require('dotenv').config();
+const express = require('express');
+const app     = express();
+const admin     = process.env.admin;
+
 
 // used to serve static files from public directory
 app.use(express.static('public'));
-app.use(express.json());
-app.use(cors());
-
-// create user account
-app.get('/account/create/:name/:email/:password', function (req, res) {
-
-    // check if account exists
-    dal.find(req.params.email).
-        then((users) => {
-
-            // if user exists, return error message
-            if(users.length > 0){
-                console.log('User already in exists');
-                res.send('User already in exists');    
-            }
-            else{
-                // else create user
-                dal.create(req.params.name,req.params.email,req.params.password).
-                    then((user) => {
-                        console.log(user);
-                        res.send(user);            
-                    });            
-            }
-
-        });
-});
 
 
-// login user 
-app.get('/account/login/:email/:password', function (req, res) {
+// function used to secure all routes
+async function verifyToken(req,res,next){
+    const idToken = req.headers.authorization;
+    console.log('idToken:', idToken);
 
-    dal.find(req.params.email).
-        then((user) => {
-
-            // if user exists, check password
-            if(user.length > 0){
-                if (user[0].password === req.params.password){
-                    res.send(user[0]);
-                }
-                else{
-                    res.send('Login failed: wrong password');
-                }
-            }
-            else{
-                res.send('Login failed: user not found');
-            }
-    });
-    
-});
-
-// find user account
-app.get('/account/find/:email', function (req, res) {
-
-    dal.find(req.params.email).
-        then((user) => {
-            console.log(user);
-            res.send(user);
-    });
-});
-
-// find one user by email - alternative to find
-app.get('/account/findOne/:email', function (req, res) {
-
-    dal.findOne(req.params.email).
-        then((user) => {
-            console.log(user);
-            res.send(user);
-    });
-});
+    if(idToken){
+        admin.auth().verifyIdToken(idToken)
+            .then(function(decodedToken) {
+                console.log('DecodedToken:',decodedToken);
+                console.log('Decoded token success!');
+                return next();
+            }).catch(function(error) {
+                console.log('Decoded token fail!');
+                return res.status(401).send('You are not authorized');
+            });
+    }
+    else{
+        console.log('Token not found!');
+        return res.status(401).send('You are not authorized');        
+    }
+}
+app.use('/auth', verifyToken);
 
 
-// update - deposit/withdraw amount
-app.get('/account/update/:email/:amount', function (req, res) {
+app.get('/open', function(req,res){
+    res.send('Open Route!');
+})
 
-    var amount = Number(req.params.amount);
+app.get('/auth', function(req,res){
+    res.send('Authentication Sucess!');
+})
 
-    dal.update(req.params.email, amount).
-        then((response) => {
-            console.log(response);
-            res.send(response);
-    });    
-});
-
-// all accounts
-app.get('/account/all', function (req, res) {
-
-    dal.all().
-        then((docs) => {
-            console.log(docs);
-            res.send(docs);
-    });
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log('Running on port: ' + port);
+app.listen(3000, () => {
+    console.log('Running on port: 3000');
+})
